e91adc3db297c7adb737b46323a8ad883746f887
Daniel <Daniel@DESKTOP-GUT2T5O>
2022-05-23 20:07:53 +0200

    "Añadida función modulo división"

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -48,5 +48,10 @@ public class Calculadora {
         double division=numero1/numero2;
         return division;
     }
+    
+    public double modulo() {
+        double modulo = numero1 % numero2;
+        return modulo;
+    }
 
 }
